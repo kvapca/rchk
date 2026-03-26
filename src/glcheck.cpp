146d6ccc98db565593333ab070636d41c245924b
Jiri Kvapil <kvapiji2@fit.cvut.cz>
2026-03-26 21:03:25 +0100

    Prepare to remove getPointerElementType from glcheck

--- src/glcheck.cpp
+++ src/glcheck.cpp
@@ -5,7 +5,9 @@
 
 #include "common.h"
 
+#include <llvm/BinaryFormat/Dwarf.h>
 #include <llvm/IR/Constants.h>
+#include <llvm/IR/DebugInfo.h>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/GlobalVariable.h>
 #include <llvm/IR/Instructions.h>
@@ -14,6 +16,7 @@
 
 #include <llvm/Support/raw_ostream.h>
 
+#include <unordered_map>
 #include <unordered_set>
 
 #include "symbols.h"
@@ -58,10 +61,101 @@ bool containsSEXP(Type *t, TypeSetTy& visited) {
   return false;
 }
 
-bool isStructureWithSEXPFields(Type *t) {
+DICompositeType* resolveForwardDeclaration(DICompositeType* t, Module* m) {
+  if (!t) return nullptr;
 
-  TypeSetTy visited;
-  return containsSEXP(t, visited);
+  // cache name and actual composite type on first run
+  static Module* cachedModule = nullptr;
+  static std::unordered_map<std::string, DICompositeType*> cache;
+  if (cachedModule == m) {
+    cachedModule = m;
+    cache.clear();
+
+    DebugInfoFinder finder;
+    finder.processModule(*m);
+
+    for (auto *type : finder.types()) {
+      if (auto *composite = dyn_cast<DICompositeType>(type)) {
+        if (!composite->isForwardDecl()) {
+          if (composite->getName() == t->getName()) {
+            return composite;
+          }
+          cache.emplace(composite->getName().str(), composite);
+        }
+      }
+    }
+  }
+
+  // retrieve from cache
+  auto it = cache.find(t->getName().str());
+  return it != cache.end() ? it->second : nullptr;
+}
+
+// return true if there is SEXP somewhere within type t
+typedef std::unordered_set<DIType*> DITypeSetTy;
+
+bool containsSEXP(DIType *t, DITypeSetTy& visited, Module* m) {
+  if (!t) return false;
+  
+  if (visited.find(t) != visited.end()) {
+    // already under evaluation (recursive type)
+    return false;
+  }
+  visited.insert(t);
+  
+  if (auto *derived = dyn_cast<DIDerivedType>(t)) {
+    return containsSEXP(derived->getBaseType(), visited, m);
+  }
+
+  if (auto *composite = dyn_cast<DICompositeType>(t)) {
+    if (composite->isForwardDecl()) {
+      return containsSEXP(resolveForwardDeclaration(composite, m), visited, m);
+    }
+
+    if (composite->getTag() == dwarf::DW_TAG_array_type)
+      return containsSEXP(composite->getBaseType(), visited, m);
+
+    if (composite->getTag() == dwarf::DW_TAG_structure_type) {
+      if (composite->getName() == "SEXPREC" || composite->getName() == "struct SEXPREC") {
+        return true;
+      }
+  
+      for (auto *element : composite->getElements()) {
+        if (containsSEXP(cast<DIType>(element), visited, m)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+bool isStructureWithSEXPFields(GlobalVariable *gv, Module* m) {
+  // TODO: remove after testing
+  TypeSetTy visited_old;
+  bool original = containsSEXP(gv->getType(), visited_old);
+  bool current = false;
+
+  DITypeSetTy visited;
+
+  SmallVector<DIGlobalVariableExpression *> debugInfoVector;
+  gv->getDebugInfo(debugInfoVector);
+
+  for (auto debugInfo : debugInfoVector) {
+    if (!debugInfo) continue;
+    DIGlobalVariable *variable = debugInfo->getVariable();
+
+    if (!variable) continue;
+    DIType *type = variable->getType();
+
+    if (containsSEXP(type, visited, m)) {
+      current = true;
+      break;
+    }
+  }
+
+  assert(current == original);
+  return current;
 }
 
 int main(int argc, char* argv[])
@@ -89,7 +183,7 @@ int main(int argc, char* argv[])
       continue;
     }
     
-    if (isStructureWithSEXPFields(gv->getType())) {
+    if (isStructureWithSEXPFields(gv, m)) {
       errs() << "structure with SEXP fields " << gv->getName() << " " << *gv << "\n";
     }
   }
