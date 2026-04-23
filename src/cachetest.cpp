39a8c50d885820a6e2add45cf847dda590896068
Jiri Kvapil <kvapiji2@fit.cvut.cz>
2026-04-23 23:59:49 +0200

    test for all builtin packages

--- src/cachetest.cpp
+++ src/cachetest.cpp
@@ -10,10 +10,55 @@
 // NOTE: As this test recomputes entire analysis, clearCommonCaches() must be called
 //       before each test to clear results from static caches
 
-std::string cacheName = "./src/main/R.bin.cache";
 std::string baseIRFile = "./src/main/R.bin.bc";
-std::string survivalIRFile = "./library/survival/libs/survival.so.bc";
+std::string cacheName = baseIRFile + ".cache";
 std::string execName = "../src/cachecheck";
+std::vector<std::string> packageIRFiles = {
+  "./.conftest.bc",
+  "./src/library/tools/src/tools.so.bc",
+  "./src/library/parallel/src/parallel.so.bc",
+  "./src/library/grid/src/grid.so.bc",
+  "./src/library/tcltk/src/tcltk.so.bc",
+  "./src/library/utils/src/utils.so.bc",
+  "./src/library/splines/src/splines.so.bc",
+  "./src/library/stats/src/stats.so.bc",
+  "./src/library/grDevices/src/grDevices.so.bc",
+  "./src/library/grDevices/src/cairo/cairo.so.bc",
+  "./src/library/graphics/src/graphics.so.bc",
+  "./src/library/methods/src/methods.so.bc",
+  "./src/modules/internet/internet.so.bc",
+  "./src/modules/lapack/lapack.so.bc",
+  "./src/modules/X11/R_X11.so.bc",
+  "./src/modules/X11/R_de.so.bc",
+  "./library/cluster/libs/cluster.so.bc",
+  "./library/rpart/libs/rpart.so.bc",
+  "./library/tools/libs/tools.so.bc",
+  "./library/class/libs/class.so.bc",
+  "./library/parallel/libs/parallel.so.bc",
+  "./library/mgcv/libs/mgcv.so.bc",
+  "./library/Matrix/libs/Matrix.so.bc",
+  "./library/KernSmooth/libs/KernSmooth.so.bc",
+  "./library/grid/libs/grid.so.bc",
+  "./library/tcltk/libs/tcltk.so.bc",
+  "./library/nnet/libs/nnet.so.bc",
+  "./library/utils/libs/utils.so.bc",
+  "./library/splines/libs/splines.so.bc",
+  "./library/stats/libs/stats.so.bc",
+  "./library/survival/libs/survival.so.bc",
+  "./library/grDevices/libs/grDevices.so.bc",
+  "./library/grDevices/libs/cairo.so.bc",
+  "./library/graphics/libs/graphics.so.bc",
+  "./library/nlme/libs/nlme.so.bc",
+  "./library/foreign/libs/foreign.so.bc",
+  "./library/lattice/libs/lattice.so.bc",
+  "./library/methods/libs/methods.so.bc",
+  "./library/MASS/libs/MASS.so.bc",
+  "./library/spatial/libs/spatial.so.bc",
+  "./modules/R_X11.so.bc",
+  "./modules/lapack.so.bc",
+  "./modules/R_de.so.bc",
+  "./modules/internet.so.bc"
+};
 
 // There are multiple interning tables involved in CalledModuleTy and ArgInfoTy:
 //  - CalledFunctionsOrderedSetTy interned via osTable
@@ -135,7 +180,7 @@ void checkRWithPackage(std::string packageIRFile) {
   clearCommonCaches();
   const char* argv[] = {execName.c_str(), "--cache", cacheName.c_str(), baseIRFile.c_str(), packageIRFile.c_str()};
   int argc = sizeof(argv) / sizeof(argv[0]);
-  errs() << "\nComparing original and restored R cache with survival package\n";
+  errs() << "\nComparing original and restored R cache with " << packageIRFile << " package\n";
   runCheck(argc, const_cast<char**>(argv));
 }
 
@@ -151,7 +196,10 @@ int main() {
   createRCacheFile(baseIRFile, cacheName);
 
   checkR();
-  checkRWithPackage(survivalIRFile);
+  
+  for (const auto& packageIRFile : packageIRFiles) {
+    checkRWithPackage(packageIRFile);
+  }
   malformedArgsCheck();
   return 0;
 }
